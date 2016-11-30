91b4d86fa91ec03efc1920f067940ee5204ddc12
layan <gino63o7@gmail.com>
2016-11-30 09:57:22 +0800

    Update for adding yee to origin

---
+++ Assets/Scripts/menu/Audio.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audio : MonoBehaviour {
+
+    AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	public void Strike()
+    {
+        audio.PlayOneShot(Resources.Load<AudioClip>("strike"));
+    }
+}
