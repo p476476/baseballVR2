c7b84ea1051d1580a5ff31ec1ae706a72f25264d
p476476 <qzawxs24489476@gmail.com>
2016-12-06 01:03:10 +0800

    123

--- Assets/Scripts/menu/MainMenu.cs
+++ Assets/Scripts/menu/MainMenu.cs
@@ -4,19 +4,46 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
 	// Use this for initialization
+
+    
+
 	void Start () {
-	
-	}
+       StartCoroutine(loadScenes());
+    }
+
+    IEnumerator loadScenes()
+    {
+
+        
+        yield return SceneManager.CreateScene("main");
+
+        yield return SceneManager.CreateScene("practice");
+
+
+    }
 	
 	// Update is called once per frame
     public void StrikeMode()
     {
-        SceneManager.LoadScene("main");
+        //StartCoroutine(loadScene("main"));
+        loadScene("main");
     }
     public void throw_mode()
     {
-        SceneManager.LoadScene("practice");
+        //SceneManager.l
+        //SceneManager.SetActiveScene(SceneManager.GetSceneByName("practice"));
+        //SceneManager.LoadScene("practice");
+        //StartCoroutine(loadScene("practice"));
+        loadScene("practice");
     }
+
+
+    void loadScene(string str)
+    {
+
+       SceneManager.LoadSceneAsync(str);
+    }
+
     public void exit()
     {
         Application.Quit();    //需確認用法
--- Assets/Scripts/menu/strike_game_btn.cs
+++ Assets/Scripts/menu/strike_game_btn.cs
@@ -5,26 +5,31 @@ public class strike_game_btn : MonoBehaviour {
 
     public ControllerBase ctrl;
     public string txt;
-
+    public ParticleSystem particle;
     Color cycle_col;
 	// Use this for initialization
 	void Start () {
         cycle_col = new Color(0.0f, 0.2f, 1f);
+        particle.GetComponent<ParticleSystemRenderer>().material.SetColor("_TintColor", cycle_col);
     }
     void OnTriggerEnter()
     {
         this.GetComponent<AudioSource>().Play();
+        cycle_col.r = 1.0f;
+        particle.GetComponent<ParticleSystemRenderer>().material.SetColor("_TintColor", cycle_col);
     }
     void OnTriggerStay(Collider col)
     {
-        cycle_col.r = 1.0f;
-        if (col.tag == "bat" && ctrl.TriggerButtonDown)
+        if (col.gameObject.tag == "Bat" && ctrl.TriggerButtonDown)
         {
+            //for test
+            print("test");
             if(txt == "start")
             {
-                CalPlayerData.Instance.init();
-                Manager.instance.gamestart = true;                      //可以投球
-                this.gameObject.transform.parent.gameObject.SetActive(false);                //將button關掉
+                CalPlayerData.Instance.init();                          //初始化玩家資料
+                Manager.instance.GameStart();                      //可以投球
+                txt = " Close ";
+                gameObject.transform.parent.gameObject.SetActive(false);                //將button關掉
             }
             else if(txt == "home")
             {
@@ -36,6 +41,7 @@ public class strike_game_btn : MonoBehaviour {
     void OnTriggerExit()
     {
         cycle_col.r = 0.0f;
+        particle.GetComponent<ParticleSystemRenderer>().material.SetColor("_TintColor", cycle_col);
     }
 
 }
