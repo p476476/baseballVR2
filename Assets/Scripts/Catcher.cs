73bbfcc453f269cdfb0dd4ec206f9d80bff73349
layan <gino63o7@gmail.com>
2016-12-07 01:26:12 +0800

    adding ball disapear when meet catcher

--- Assets/Scripts/Catcher.cs
+++ Assets/Scripts/Catcher.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class Catcher : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    GameObject admin;
+    // Use this for initialization
+    void Awake() { 
+        admin = GameObject.FindGameObjectWithTag("Admin");
+    }
 	
 	// Update is called once per frame
 
-	void OnCollisionEnter(Collision collision)
+	void OnTriggerEnter(Collider collision)
 	{
 		if (collision.gameObject.tag == "Ball") 
 		{
             if (collision.gameObject.GetComponent<Ball>().state == Ball.State.Flying1)
+            {
+                admin.GetComponent<CalPlayerData>().lifeUpdate(false);
                 Destroy(collision.gameObject);
+            }
+                
 		}
 	}
 }
